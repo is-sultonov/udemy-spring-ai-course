552219a05a16fbf131f2ab365332bd959c4e940e
isfandiyorsultonov <i.sultonov@octo.uz>
2025-09-03 17:13:33 +0500

    Chroma db demo project for vector-store was completed

---
+++ vector-database-cromadb/src/test/java/org/spring/ai/vectordatabse/VectorDatabaseCromadbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.ai.vectordatabse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VectorDatabaseCromadbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
