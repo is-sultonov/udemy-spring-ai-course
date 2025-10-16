b5de30c7fd51fbde81228bef08f9edd2903c1074
Isfandiyor <82582331+is-sultonov@users.noreply.github.com>
2025-10-16 09:39:50 +0500

    Spring text-to-speech api learning project was completed (#5)
    Co-authored-by: isfandiyorsultonov <i.sultonov@octo.uz>

---
+++ text-to-speech-openai/src/main/java/org/spring/ai/speech/generation/TextToSpeechOpenaiApplication.java
@@ -0,0 +1,13 @@
+package org.spring.ai.speech.generation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextToSpeechOpenaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextToSpeechOpenaiApplication.class, args);
+    }
+
+}
