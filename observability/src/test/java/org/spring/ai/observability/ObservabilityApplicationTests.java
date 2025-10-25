a749fd734523e2434778a225c3759c9a79b3287c
isfandiyorsultonov <i.sultonov@octo.uz>
2025-10-25 17:28:29 +0500

    Spring ai observability example completed

---
+++ observability/src/test/java/org/spring/ai/observability/ObservabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.ai.observability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservabilityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
